c64db1abb264810dab55968a292662beb94e6b34
JMG <jmg@JMGs-Air.bbrouter>
2020-10-18 04:37:29 +0800

    CF

--- public/contollers/cfnewCtrl.js
+++ public/contollers/cfnewCtrl.js
@@ -56,4 +56,54 @@ angular.module('newApp').controller('cfnewCtrl', function($scope) {
         console.log(updates)
 
     });
+    var cnt = 0;
+    $scope.addtr = function() {
+        $("#appendhere").append('<tr><td> <label class="input"><input type="text" name="particulars" ></label></td><td><select name="interested " id="type" required class="form-control btn-block"><option value="0 "  disabled=" ">Select</option><option value="1" selected="true">Miscellaneous Fee</option><option value="2">Tuition Fee</option><option value="3">Laboratory/Workshop Fee</option><option value="4">Uniforms</option><option value="5">Logo</option><option value="6">Credentials</option><option value="7">Others</option></select></td><td><label class="input"> <input type="text" name="remarks" placeholder="Remarks"></label></td></td><td class="col-md-2"><label class="input col-md-10"><input type="text" name="amount" value="" class="txt" autocomplete="off" required/></label><label class="input col-md-1"><button class="btn btn-danger btn-sm deleteb">X</button></label></td></tr>');
+        cnt++;
+        $('table thead th').each(function(i) {
+
+        });
+
+    }
+    $scope.removetr = function() {
+
+        $('#appendhere tr:last').remove();
+        $('table thead th').each(function(i) {
+
+        });
+
+    }
+
+    $("#appendhere").on('click', '.deleteb', function() {
+        $(this).closest("tr").remove();
+        $('table thead th').each(function(i) {
+
+        });
+    });
+
+
+    function calculateSum() {
+        var sum = 0;
+        //iterate through each textboxes and add the values
+        $(".txt").each(function() {
+
+            //add only if the value is number
+            if (!isNaN(this.value) && this.value.length != 0) {
+                sum += parseFloat(this.value);
+            }
+
+        });
+        //.toFixed() method will roundoff the final sum to 2 decimal places
+        $("#sum").html(sum.toFixed(2));
+        console.log(sum.toFixed(2));
+    }
+
+    $("#convert-table").on("keyup", ".txt", function() {
+        calculateSum();
+    });
+
+
+
+
+
 });
